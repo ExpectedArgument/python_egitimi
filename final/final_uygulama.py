22e3245566c146bd16f40be1f0c52df95f946a5c
muvahhid <muvahhidkilic@gmail.com>
2023-01-02 13:50:09 +0300

    update

--- final/final_uygulama.py
+++ final/final_uygulama.py
@@ -1,22 +1,26 @@
-from final.left_right_lane import left_right_lane as _
+from left_right_lane import left_right_lane as _
 import cv2
 
-# imgcv2 = cv2.imread('lane1.png')
-# l = _()
-# l.do(imgcv2)
-# cv2.imwrite('lane1_out.png', imgcv2)
-
-# read video
+# read photo 
+imgcv2 = cv2.imread('lane1.png')
 l = _()
-cap = cv2.VideoCapture('driving.mp4')
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        # process frame
-        try:
-            l.do(frame)
-        except :
-            pass
-    else:
-        break
+l.do(imgcv2)
+cv2.imshow('img', imgcv2)
+cv2.waitKey(0)
+cv2.imwrite('lane1_out.png', imgcv2)
+
+
+# # read video
+# l = _()
+# cap = cv2.VideoCapture('driving.mp4')
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if ret:
+#         # process frame
+#         try:
+#             l.do(frame)
+#         except :
+#             pass
+#     else:
+#         break
 
